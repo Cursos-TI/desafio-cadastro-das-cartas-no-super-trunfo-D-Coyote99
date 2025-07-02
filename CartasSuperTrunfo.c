1264eae705829d0d1c916ca5254344e6c1e809aa
D-Coyote99 <denaico2009@gmail.com>
2025-07-02 21:14:44 +0000

    desafio_aventureiro

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -5,7 +5,7 @@ int main(){
     char estado1[30], estado2[30];
     char codigo_carta1[5], codigo_carta2[5], nome_cidade1[30], nome_cidade2[30];
     int populacao1, populacao2, pontos_turistico1, pontos_turistico2;
-    float area1, area2, pib1, pib2;
+    float area1, area2, pib1, pib2, densidade1, densidade2, perCapta1, perCapta2;
 
 
     // Entrada do usuário para carta1
@@ -56,6 +56,15 @@ int main(){
     scanf("%f", &pib2);
     printf("\n");
 
+    // Formula para calculo da densidade e renda
+    // Carta 1
+    densidade1 = populacao1 / area1;
+    perCapta1 = pib1 / populacao1;
+
+    // Carta 2
+    densidade2 = populacao2 / area2;
+    perCapta2 = pib2 / populacao2;
+
     //Exibindo os dados das cartas
     printf("\nCarta1");
     printf("\nEstado: %s", estado1);
@@ -65,7 +74,10 @@ int main(){
     printf("\nÁrea em km²: %f", area1);
     printf("\nPib: %f", pib1);
     printf("\nNúmero de pontos turísticos: %d", pontos_turistico1);
+    printf("\nDensidade populacional: %f hab/km²", densidade1);
+    printf("\nPib PerCapta: %f Reais", perCapta1);
     printf("\n");
+
     printf("\nCarta2");
     printf("\nEstado: %s", estado2);
     printf("\nCódigo da carta: %s", codigo_carta2);
@@ -74,6 +86,9 @@ int main(){
     printf("\nÁrea em km²: %f", area2);
     printf("\nPib: %f", pib2);
     printf("\nNúmero de pontos turísticos: %d", pontos_turistico2);
+    printf("\nDensidade populacional: %f hab/km²", densidade2);
+    printf("\nPib PerCapta: %f Reais", perCapta2);
+    printf("\n");
 
     return 0;
 
