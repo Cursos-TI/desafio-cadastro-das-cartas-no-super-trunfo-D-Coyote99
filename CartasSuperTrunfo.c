99265f1cb505b6574975df5ba5c4eced18f879fb
D-Coyote99 <denaico2009@gmail.com>
2025-07-04 19:23:42 +0000

    Nivel_Avançado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,7 +4,9 @@ int main(){
     // Declarando as variáveis
     char estado1[30], estado2[30];
     char codigo_carta1[5], codigo_carta2[5], nome_cidade1[30], nome_cidade2[30];
-    int populacao1, populacao2, pontos_turistico1, pontos_turistico2;
+    unsigned long int populacao1, populacao2;
+    float superPoder1, superPoder2;
+    int pontos_turistico1, pontos_turistico2;
     float area1, area2, pib1, pib2, densidade1, densidade2, perCapta1, perCapta2;
 
 
@@ -20,7 +22,7 @@ int main(){
     scanf("%s", &nome_cidade1);
 
     printf("Digite o total da população: \n");
-    scanf("%d", &populacao1);
+    scanf("%lu", &populacao1);
 
     printf("Digite a quantidade de pontos turisticos: \n");
     scanf("%d", &pontos_turistico1);
@@ -44,7 +46,7 @@ int main(){
     scanf("%s", &nome_cidade2);
 
     printf("Digite o total da população: \n");
-    scanf("%d", &populacao2);
+    scanf("%lu", &populacao2);
 
     printf("Digite a quantidade de pontos turisticos: \n");
     scanf("%d", &pontos_turistico2);
@@ -59,36 +61,50 @@ int main(){
     // Formula para calculo da densidade e renda
     // Carta 1
     densidade1 = populacao1 / area1;
-    perCapta1 = pib1 / populacao1;
+    perCapta1 = (unsigned long int) pib1 / populacao1;
+    superPoder1 += (populacao1 + pontos_turistico1 + area1 + pib1 + perCapta1) - densidade1;
 
     // Carta 2
     densidade2 = populacao2 / area2;
-    perCapta2 = pib2 / populacao2;
+    perCapta2 =(unsigned long int) pib2 / populacao2;
+    superPoder2 = (populacao2 + pontos_turistico2 + area2 + pib2 + perCapta2) - densidade2;
 
     //Exibindo os dados das cartas
     printf("\nCarta1");
     printf("\nEstado: %s", estado1);
     printf("\nCódigo da carta: %s", codigo_carta1);
     printf("\nNome da cidade: %s", nome_cidade1);
-    printf("\nPopulação: %d", populacao1);
-    printf("\nÁrea em km²: %f", area1);
-    printf("\nPib: %f", pib1);
+    printf("\nPopulação: %lu", populacao1);
+    printf("\nÁrea em km²: %.2f", area1);
+    printf("\nPib: %.2f", pib1);
     printf("\nNúmero de pontos turísticos: %d", pontos_turistico1);
-    printf("\nDensidade populacional: %f hab/km²", densidade1);
-    printf("\nPib PerCapta: %f Reais", perCapta1);
+    printf("\nDensidade populacional: %.2f hab/km²", densidade1);
+    printf("\nPib PerCapta: %.2f Reais", perCapta1);
+    printf("\nSuper Poder: %f", superPoder1);
     printf("\n");
 
     printf("\nCarta2");
     printf("\nEstado: %s", estado2);
     printf("\nCódigo da carta: %s", codigo_carta2);
     printf("\nNome da cidade: %s", nome_cidade2);
-    printf("\nPopulação: %d", populacao2);
-    printf("\nÁrea em km²: %f", area2);
-    printf("\nPib: %f", pib2);
+    printf("\nPopulação: %lu", populacao2);
+    printf("\nÁrea em km²: %.2f", area2);
+    printf("\nPib: %.2f", pib2);
     printf("\nNúmero de pontos turísticos: %d", pontos_turistico2);
-    printf("\nDensidade populacional: %f hab/km²", densidade2);
-    printf("\nPib PerCapta: %f Reais", perCapta2);
+    printf("\nDensidade populacional: %.2f hab/km²", densidade2);
+    printf("\nPib PerCapta: %.2f Reais", perCapta2);
+    printf("\nSuper Poder: %f", superPoder2);
     printf("\n");
+
+    // Comparando as cartas
+    printf("\n*** Comparação das Cartas ***\n");
+    printf("\nPopulação: Carta 1 (%d) Carta 2 (%d)", populacao1 > populacao2, populacao1 < populacao2);
+    printf("\nÁrea: Carta 1 (%d) Carta 2 (%d)", area1 > area2, area1 < area2);
+    printf("\nPib: Carta 1 (%d) Carta 2 (%d)", pib1 > pib2, pib1 < pib2);
+    printf("\nPontos Turísticos: Carta 1 (%d) Carta 2 (%d)", pontos_turistico1 > pontos_turistico2, pontos_turistico1 < pontos_turistico2);
+    printf("\nDensidade Populacional: Carta 1 (%d) Carta 2 (%d)", densidade1 < densidade2, densidade1 > densidade2);
+    printf("\nPib Per Capta: Carta 1 (%d) Carta 2 (%d)", perCapta1 > perCapta2, perCapta1 < perCapta2);
+    printf("\nSuper Poder: Carta 1 (%d) Carta 2 (%d)", superPoder1 > superPoder2, superPoder1 < superPoder2);
 
     return 0;
 
